6de5c45a79d3a27ae274fa74e0c93d4f13b61aba
Caleb Babin <object.caleb@gmail.com>
2021-08-25 16:42:58 -0500

    add new streetsigns

--- src/biomes/streetsigns.js
+++ src/biomes/streetsigns.js
@@ -1,5 +1,11 @@
 const streetSignDecoration = {
-    imageUrl: ['/sprites/streetsign_NaM.png', '/sprites/streetsign_borpa.png', '/sprites/streetsign_ratio.png'],
+    imageUrl: [
+        '/sprites/streetsign_NaM.png',
+        '/sprites/streetsign_borpa.png',
+        '/sprites/streetsign_ratio.png',
+        '/sprites/streetsign_kkonacounty.png',
+        '/sprites/streetsign_nakedcowboys.png',
+    ],
     interval: 90000,
     intervalVariance: 0.5,
     spawnDistanceMultiplier: 0,
